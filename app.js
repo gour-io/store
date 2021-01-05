c6ec9b55a533b856b1399d543db44220f1a44267
gour-io <gour-io.github.com>
2021-01-05 19:45:37 +0530

    implement EJS to application

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const expressHbs = require('express-handlebars')
 
 const rootDir = require('./utils/path')
 
@@ -14,13 +13,7 @@ const app = express()
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-// Setting PUG template engine globally to our express application
-// app.set('view engine', 'pug');
-// app.set('views', 'views');
-
-// Setting handlebars template engine
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}))
-app.set('view engine', 'hbs')
+app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 // Serving static files
@@ -30,7 +23,7 @@ app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {docTitle: '404 Not Found'})
+    res.status(404).render('404', {docTitle: '404 Not Found', path})
     // res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
 })
 
